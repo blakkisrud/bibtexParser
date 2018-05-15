7979de2ce3ca8f4c21760954ff728e60db0fff08
Johan <blakkisrud@gmail.com>
2018-05-15 16:21:58 +0200

    Touching up on file

--- replace_script.py
+++ replace_script.py
@@ -7,12 +7,13 @@ TODO: Write argparser in the form
 python replace_script [file_bibtex] [file_beamer] [file_output] - now too tired
 """
 
-import numpy as np
 from funcs import make_article_element
 from funcs import make_list_of_reference_dicts
 from funcs import replace_labels_to_foot_notes
 
 
-#replace_labels_to_foot_notes(bibtex_file="bib_nsnm_siemens.txt",
-#                            beamer_file = "beamer.tex",
-#                            new_file_path = "new_new_test.tex")
+replace_labels_to_foot_notes(bibtex_file="bib_nsnm_siemens.txt",
+                            beamer_file = "beamer.tex",
+                            new_file_path = "new_new_test.tex")
+
+print "Done!"
